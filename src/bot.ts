ed847c0e1d36419a59db7f719572b4c2a67d3a24
Scott Babcock <scott.babcock@onsharp.com>
2021-07-08 10:03:49 -0500

    Moves hardcoded values to settings.

--- src/bot.ts
+++ src/bot.ts
@@ -1,7 +1,7 @@
-import { api, driver } from '@rocket.chat/sdk';
+import { driver } from '@rocket.chat/sdk';
 import dotenv from 'dotenv';
 
-import { CommandHandler } from './commands/CommandHandler';
+import { getCommandHandler } from './commands/CommandHandler';
 
 dotenv.config();
 
@@ -9,10 +9,12 @@ const {
 	ROCKETCHAT_URL,
 	ROCKETCHAT_USER,
 	ROCKETCHAT_PASSWORD,
-	ROCKETCHAT_USE_SSL
+	ROCKETCHAT_USE_SSL,
+	BOT_PREFIX,
+	BOT_ROOM
 } = process.env;
 
-if (!ROCKETCHAT_URL || !ROCKETCHAT_USER || !ROCKETCHAT_PASSWORD) {
+if (!ROCKETCHAT_URL || !ROCKETCHAT_USER || !ROCKETCHAT_PASSWORD || !BOT_PREFIX || !BOT_ROOM) {
 	console.error('Missing required environment variables.');
 	process.exit(1);
 }
@@ -21,11 +23,10 @@ if (!ROCKETCHAT_URL || !ROCKETCHAT_USER || !ROCKETCHAT_PASSWORD) {
 	// Connect to server and login.
 	await driver.connect({ host: ROCKETCHAT_URL, useSsl: !!ROCKETCHAT_USE_SSL });
 	await driver.login({ username: ROCKETCHAT_USER, password: ROCKETCHAT_PASSWORD });
-	// await api.login({ username: ROCKETCHAT_USER, password: ROCKETCHAT_PASSWORD });
 
 	// Join rooms and send welcome message.
-	await driver.joinRooms(['general']);
+	await driver.joinRooms([BOT_ROOM]);
 	await driver.subscribeToMessages();
-	driver.reactToMessages(CommandHandler);
-	await driver.sendToRoom("I'm alive!", 'general');
+	driver.reactToMessages(getCommandHandler({ prefix: BOT_PREFIX }));
+	await driver.sendToRoom("I'm alive!", BOT_ROOM);
 })();
--- src/commands/CommandHandler.ts
+++ src/commands/CommandHandler.ts
@@ -1,29 +1,38 @@
 import { driver } from '@rocket.chat/sdk';
+import { ICallback } from '@rocket.chat/sdk/dist/config/driverInterfaces';
 import { IMessage } from '@rocket.chat/sdk/dist/config/messageInterfaces';
 
 import { CommandList } from './CommandList';
 
-export const CommandHandler = async (err: unknown, ...messages: IMessage[]): Promise<void> => {
-	if (err) {
-		console.error(err);
-		return;
-	}
+export interface ICommandHandlerOptions {
+	prefix: string;
+}
 
-	const message = messages[0];
-	if (!message?.msg || !message?.rid) {
-		console.error('Invalid message received.', message);
-		return;
-	}
+export function getCommandHandler(options: ICommandHandlerOptions): ICallback {
+	return async (err: unknown, ...messages: IMessage[]): Promise<void> => {
+		if (err) {
+			console.error(err);
+			return;
+		}
 
-	const roomName = await driver.getRoomName(message.rid);
-	const [prefix, commandName] = message.msg.split(' ');
-	if (prefix === '!fCC') {
+		const message = messages[0];
+		if (!message?.msg || !message?.rid) {
+			console.error('Invalid message received.', message);
+			return;
+		}
+
+		const roomName = await driver.getRoomName(message.rid);
+		const [prefix, commandName] = message.msg.split(' ');
+		if (prefix !== options.prefix ) {
+			return;
+		}
+
 		for (const Command of CommandList) {
 			if (commandName === Command.name) {
 				await Command.command(message, roomName);
 				return;
 			}
 		}
-		await driver.sendToRoom(`I am sorry, but '${commandName}'' is not a valid command.`, roomName);
+		await driver.sendToRoom(`I am sorry, but \`${commandName}\` is not a valid command.`, roomName);
 	}
-};
+}
