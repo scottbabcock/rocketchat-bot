c432920aacbf9313cbf0c643737d6c4cf364f243
Scott Babcock <scott.babcock@onsharp.com>
2021-07-08 11:29:52 -0500

    Adds parameters and usage to commands.

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -7,9 +7,49 @@ import { ICommand } from '../interfaces';
 export const help: ICommand = {
 	name: 'help',
 	description: `Lists information on the bot's list of commands.`,
-	command: async (_message, room) => {
-		const commandList: string[] = CommandList.map(command => `\`${BOT.prefix} ${command.name}\`: ${command.description}`);
-		const response: string = `Hello! I am chat bot created for this server! Here is the list of available commands:\n${commandList.sort().join('\n')}`
-		await driver.sendToRoom(response, room);
+	parameters: ['?command'],
+	usage: [
+		"`{prefix} help` - will return a list of commands.",
+		"`{prefix} help ?command` - will return a detailed description of that command.",
+	],
+	command: async (message, room) => {
+		const [query] = message.msg!.split(" ").slice(2);
+		await driver.sendToRoom(getResponse(query), room);
+
+		function getResponse(query: string): string {
+			if (!query) {
+				const commandList: string[] = CommandList.map(command => `\`${BOT.prefix} ${command.name}\`: ${command.description}`);
+				return `Hello! I am chat bot created for this server! Here is the list of available commands:\n${commandList.sort().join('\n')}`;
+			}
+
+			const targetCommand = CommandList.find(command => command.name === query);
+			if (!targetCommand) {
+				return `I'm sorry, but I do not have a \`${query}\` command.`;
+			}
+
+			return getCommandResponse(targetCommand);
+		}
+
+		function getCommandResponse(command: ICommand): string {
+			let response = `*Information on my \`${command.name}\` command:*
+_Description_
+${command.description}`;
+
+			if (command.parameters?.length) {
+				response += `
+
+_Parameters_
+${command.parameters?.join(" ") ?? "none"}`;
+			}
+
+			if (command.usage?.length) {
+				response += `
+
+_Example Uses_
+${command.usage?.map((use) => use.replace(/\{prefix\}/g, BOT.prefix)).join("\n")}`;
+			}
+
+			return response;
+		}
 	}
-}
+};
--- src/interfaces/ICommand.ts
+++ src/interfaces/ICommand.ts
@@ -3,5 +3,7 @@ import { IMessage } from '@rocket.chat/sdk/dist/config/messageInterfaces';
 export interface ICommand {
 	name: string;
 	description: string;
+	parameters?: string[];
+	usage?: string[];
 	command: (message: IMessage, room: string) => Promise<void>;
 }
