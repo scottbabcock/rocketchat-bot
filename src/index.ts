4703d0d0ea17dd8b32e25e1059818e4cb7608c4f
Scott Babcock <scott.babcock@onsharp.com>
2021-07-09 09:32:15 -0500

    Refactors entry point.

--- src/index.ts
+++ src/index.ts
@@ -21,21 +21,27 @@ if (!ROCKETCHAT_URL || !ROCKETCHAT_USER || !ROCKETCHAT_PASSWORD || !BOT_NAME) {
 	process.exit(1);
 }
 
+// Global instance of bot for reference by commands.
 export const BOT: IBot = {
 	name: BOT_NAME,
 	prefix: BOT_PREFIX,
 };
 
-(async () => {
+// Starts the bot.
+const runBot = async () => {
 	// Connect to server and login.
 	await driver.connect({ host: ROCKETCHAT_URL, useSsl: !!ROCKETCHAT_USE_SSL });
 	await driver.login({ username: ROCKETCHAT_USER, password: ROCKETCHAT_PASSWORD });
 
-	// Join rooms and send welcome message.
+	// Join rooms.
 	await driver.joinRooms(ROOMS);
 	await driver.subscribeToMessages();
 	driver.reactToMessages(CommandHandler);
+
+	// Send welcome messages.
 	for (const room of ROOMS) {
 		await driver.sendToRoom(`${BOT_NAME} is online. Use \`${BOT_PREFIX}\` to send commands.`, room);
 	}
-})();
+};
+
+runBot();
